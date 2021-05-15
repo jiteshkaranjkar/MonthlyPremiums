b640d6e91820f7c5d58681eaee13fdc1cbe05950
jk <jiteshkaranjkar@gmail.com>
2021-05-16 01:34:12 +1000

    Added UI and Unit/Integration test Project

---
+++ MonthlyPremiums.Web/ClientApp/src/components/DatePickers.js
@@ -0,0 +1,43 @@
+﻿import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    width: 400,
+    marginLeft:10,
+  },
+}));
+
+
+export default function DatePickers() {
+  const [dateValue, setDateValue] = useState(new Date().toLocaleDateString('en-US'));
+  const classes = useStyles();
+
+  useEffect(() => {
+    //setDateValue(/*dateValue('MM', Date().getMonth() + 1));*/
+
+  })
+
+  const handleDateChange = (event) => {
+    console.log(event.target.value);
+    setDateValue(event.target.value)
+  }
+
+  return (
+    <TextField
+      id="date"
+      variant="outlined"
+      label="DOB"
+      value={dateValue}
+      onChange={handleDateChange}
+      type="date"
+      className={classes.container}
+      InputLabelProps={{
+        shrink: true,
+      }}
+    />
+  );
+}
